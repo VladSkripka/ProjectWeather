4a061acd1f944cfd8c58d6d6a83e7ae475aa6fa9
VladSkripka <84273460+VladSkripka@users.noreply.github.com>
2021-05-22 15:12:08 +0300

    Add files via upload

--- Controller.java
+++ Controller.java
@@ -1,19 +1,48 @@
 package sample;
 
+
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
 
 public class Controller {
 
     @FXML
-    private ResourceBundle resources;
+    public ResourceBundle resources;
 
     @FXML
-    private URL location;
+    public URL location;
+
+    @FXML
+    private Button getData;
+
+    @FXML
+    public TextField city;
+
+    @FXML
+    public Text temperature;
+
+    @FXML
+    public Text feelslike;
+
+    @FXML
+    public Text visibility;
+
+    @FXML
+    public Text probablyOfRain;
+
+    @FXML
+    public Text pressure;
 
     @FXML
     void initialize() {
+        getData.setOnAction(actionEvent -> {
+            System.out.print("JR");
 
+        });
     }
 }
